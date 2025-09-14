2a22fcd9f379dad2751b6ef62a3914350932a370
Admin <Admin@DESKTOP-48IJP0F.mshome.net>
2025-09-14 10:15:28 +0530

    Update with Extent Report in Demo2

--- src/test/java/day22/Demo2.java
+++ src/test/java/day22/Demo2.java
@@ -6,25 +6,36 @@ import org.openqa.selenium.By;
 import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class Demo1 {
+public class Demo2 {
 
 	@Parameters({"gridURL","appURL"})
 	@Test
 	public void testA(String gridURL,String appURL) throws Exception
 	{
-		Reporter.log("TestA of Demo1 Of Day22",true);
+		ExtentReports report=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter("report/spark.html");
+		report.attachReporter(spark);
+		ExtentTest extentTest=report.createTest("testA");
+		extentTest.info("testA of Demo2 of day 22");
 		WebDriver driver=new RemoteWebDriver(new URL(gridURL),new ChromeOptions());
 		driver.get(appURL);
-		System.out.println(driver.getTitle());
+		extentTest.info(driver.getTitle());
+		extentTest.info("Enter username,pw & click login");
 		driver.findElement(By.id("input-username")).sendKeys("admin");
 		driver.findElement(By.id("input-password")).sendKeys("pointofsale");
 		driver.findElement(By.name("login-button")).click();
 		Thread.sleep(7000);
-		System.out.println(driver.getTitle());
+		extentTest.info(driver.getTitle());
+		report.flush();
 	}
 }
